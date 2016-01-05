a9046d52fc6ea225f4e71a63f10780ad86480524
Fher Eduardo <fhereduardo90@gmail.com>
2016-01-04 23:52:01 -0600

    Add validations.

--- app/models/user.js
+++ app/models/user.js
@@ -7,19 +7,32 @@ module.exports = sequelizeConnection.define('user', {
   name: {
     type: DataTypes.STRING,
     allowNull: false,
+    validate: {
+      notEmpty: true
+    }
   },
   email: {
     type: DataTypes.STRING,
     allowNull: false,
-    unique: true
+    unique: true,
+    validate: {
+      isEmail: true,
+      notEmpty: true
+    }
   },
   identifier: {
     type: DataTypes.STRING,
     allowNull: false,
-    unique:true
+    unique:true,
+    validate: {
+      notEmpty: true
+    }
   },
   birthdate: {
-    type: DataTypes.DATE
+    type: DataTypes.DATE,
+    validate: {
+      isDate: true
+    }
   }
 }, {
   underscored: true
