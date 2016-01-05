3c6b5eb12e5721ec474be00c8db4ea7821dc0506
Fher Eduardo <fhereduardo90@gmail.com>
2016-01-04 22:51:25 -0600

    Create service "createUser" and add function generateIdentifier

--- app/services/users/create_user.js
+++ app/services/users/create_user.js
@@ -1,11 +1,28 @@
 var User    = require('../../models/user');
+var nid     = require('nid');
+var lodash  = require('lodash');
 
 module.exports.call = function(params, cb) {
-  User.build(params).save()
-    .then(function(user){
-      cb({result: user, success: true, errors: []});
-    })
-    .catch(function(err){
-      cb({result: null, success: false, errors: err.errors})
-    });
+  var user = User.build(params);
+  generateIdentifier(user, cb);
 };
+
+function generateIdentifier(user, cb){
+  var newIdentifier = nid({hex: 6}).call();
+
+  User.count({where: {identifier: newIdentifier}})
+    .then(function(c){
+      if(c >  0){
+        generateIdentifier(user, cb);
+      }else{
+        user.identifier = newIdentifier;
+        user.save()
+          .then(function(user){
+            cb({result: user, success: true, errors: []});
+          })
+          .catch(function(err){
+            cb({result: null, success: false, errors: err.errors});
+          });
+      }
+    });
+}
