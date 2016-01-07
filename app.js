aafbf56944478812ef96245557957074217d4294
Fher Eduardo <fhereduardo90@gmail.com>
2016-01-06 23:59:51 -0600

    Include CompanyController.

--- app.js
+++ app.js
@@ -7,7 +7,8 @@ var port            = process.env.PORT;
 var router          = express.Router();
 
 // Controllers
-var UserController = require('./app/controllers/users');
+var UserController      = require('./app/controllers/users');
+var CompanyController   = require('./app/controllers/companies');
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
@@ -16,6 +17,6 @@ router.get('/', function(req, res){
   res.json({ message: 'CLIFRE API :)' });
 });
 
-app.use([router, UserController]);
+app.use([router, UserController, CompanyController]);
 
 app.listen(port);
