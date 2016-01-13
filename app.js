3565e06013c14a82bab1a1c71a2de0c133b5ae4f
Fher Eduardo <fhereduardo90@gmail.com>
2016-01-12 23:41:12 -0600

    add app.js.

--- app.js
+++ app.js
@@ -1,1 +1,22 @@
-web: node app.js
+require('dotenv').load();
+
+var express         = require('express');
+var app             = express();
+var bodyParser      = require('body-parser');
+var port            = process.env.PORT;
+var router          = express.Router();
+
+// Controllers
+var UserController      = require('./app/controllers/users');
+var CompanyController   = require('./app/controllers/companies');
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+router.get('/', function(req, res){
+  res.json({ message: 'CLIFRE API :)' });
+});
+
+app.use([router, UserController, CompanyController]);
+
+app.listen(port);
