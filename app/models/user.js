f333736f7760435724b904d22665057be1075386
Fher Eduardo <fhereduardo90@gmail.com>
2016-01-25 22:40:38 -0600

    Update structure.

--- app/models/user.js
+++ app/models/user.js
@@ -1,39 +1,41 @@
 'use strict';
 
-var sequelizeConnection = require('../../config/database');
-var DataTypes = require('sequelize');
+module.exports = function(sequelize, DataTypes) {
+  var User = sequelize.define('User', {
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        notEmpty: true
+      }
+    },
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+      validate: {
+        isEmail: true,
+        notEmpty: true
+      }
+    },
+    identifier: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique:true,
+      validate: {
+        notEmpty: true
+      }
+    },
+    birthdate: {
+      type: DataTypes.DATE,
+      validate: {
+        isDate: true
+      }
+    }
+  }, {
+    underscored: true,
+    tableName: 'users'
+  });
 
-module.exports = sequelizeConnection.define('user', {
-  name: {
-    type: DataTypes.STRING,
-    allowNull: false,
-    validate: {
-      notEmpty: true
-    }
-  },
-  email: {
-    type: DataTypes.STRING,
-    allowNull: false,
-    unique: true,
-    validate: {
-      isEmail: true,
-      notEmpty: true
-    }
-  },
-  identifier: {
-    type: DataTypes.STRING,
-    allowNull: false,
-    unique:true,
-    validate: {
-      notEmpty: true
-    }
-  },
-  birthdate: {
-    type: DataTypes.DATE,
-    validate: {
-      isDate: true
-    }
-  }
-}, {
-  underscored: true
-});
+  return User;
+}
