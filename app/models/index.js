c02c2e9c26bc4771c40e0d77b597296aba6759d1
Fher Eduardo <fhereduardo90@gmail.com>
2016-01-25 22:40:03 -0600

    Update configuration.

--- app/models/index.js
+++ app/models/index.js
@@ -5,7 +5,7 @@ var path      = require('path');
 var Sequelize = require('sequelize');
 var basename  = path.basename(module.filename);
 var env       = process.env.NODE_ENV || 'development';
-var config    = require(__dirname + '/../../config/config.json')[env];
+var config    = require(__dirname + '/../../config/config.js')[env];
 var db        = {};
 
 if (config.use_env_variable) {
@@ -17,15 +17,15 @@ if (config.use_env_variable) {
 fs
   .readdirSync(__dirname)
   .filter(function(file) {
-    return (file.indexOf('.') !== 0) && (file !== basename) && (file.slice(-3) === '.js');
+    return (file.indexOf(".") !== 0) && (file !== "index.js");
   })
   .forEach(function(file) {
-    var model = sequelize['import'](path.join(__dirname, file));
+    var model = sequelize.import(path.join(__dirname, file));
     db[model.name] = model;
   });
 
 Object.keys(db).forEach(function(modelName) {
-  if (db[modelName].associate) {
+  if ("associate" in db[modelName]) {
     db[modelName].associate(db);
   }
 });
