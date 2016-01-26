db0560305f1489bd7f7e3ad376e976cc24ad80bb
Fher Eduardo <fhereduardo90@gmail.com>
2016-01-25 22:39:42 -0600

    Add relationship hasMany with card model.

--- app/models/company.js
+++ app/models/company.js
@@ -1,40 +1,46 @@
 'use strict';
 
-var sequelizeConnection = require('../../config/database');
-var DataTypes = require('sequelize');
-
-module.exports = sequelizeConnection.define('company', {
-  name: {
-    type: DataTypes.STRING,
-    allowNull: false,
-    validate: {
-      notEmpty: true
+module.exports = function(sequelize, DataTypes) {
+  var Company = sequelize.define('Company', {
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        notEmpty: true
+      }
+    },
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+      validate: {
+        isEmail: true,
+        notEmpty: true
+      }
+    },
+    about: {
+      type: DataTypes.STRING,
+    },
+    address: {
+      type: DataTypes.STRING,
+    },
+    phone: {
+      type: DataTypes.STRING,
+    },
+    avatar_name: {
+      type: DataTypes.STRING,
+    },
+    avatar_path: {
+      type: DataTypes.STRING,
     }
-  },
-  email: {
-    type: DataTypes.STRING,
-    allowNull: false,
-    unique: true,
-    validate: {
-      isEmail: true,
-      notEmpty: true
+  }, {
+    underscored: true,
+    tableName: 'companies',
+    classMethods: {
+      associate: function(models) {
+        Company.hasMany(models.Card, {as: 'Cards'});
+      }
     }
-  },
-  about: {
-    type: DataTypes.STRING,
-  },
-  address: {
-    type: DataTypes.STRING,
-  },
-  phone: {
-    type: DataTypes.STRING,
-  },
-  avatar_name: {
-    type: DataTypes.STRING,
-  },
-  avatar_path: {
-    type: DataTypes.STRING,
-  }
-}, {
-  underscored: true
-});
+  });
+  return Company;
+}
