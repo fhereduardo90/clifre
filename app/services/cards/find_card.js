a17c8814c0c002f0dc20a88bcaeb8a072f9d079e
Fher Eduardo <fhereduardo90@gmail.com>
2016-01-31 21:48:29 -0600

    Add service to retrieve an specific card.

--- app/services/cards/find_card.js
+++ app/services/cards/find_card.js
@@ -1,10 +1,9 @@
 var sequelize     = require('../../models');
 var _             = require('lodash');
 
-module.exports.call = function(companyId, cb) {
-  if (_.isEmpty(companyId)) {
-    companyId = 0;
-  }
+module.exports.call = function(companyId, id, cb) {
+  if (_.isEmpty(companyId)) companyId = 0;
+  if (_.isEmpty(id)) id = 0;
 
   return sequelize.Company.findById(parseInt(companyId), {attributes: ['id']})
     .then(function(company) {
@@ -13,9 +12,17 @@ module.exports.call = function(companyId, cb) {
           message: 'Company not found.', errors: []});
       }
 
-      return company.getCards({attributes: ['title', 'seals', 'description', 'color', 'company_id']})
-        .then(function(cards) {
-          return cb({result: cards, status: 200, success: true, message: '', errors: []});
+      return sequelize.Card.findOne({
+        where: {id: parseInt(id), company_id: company.id},
+        attributes: ['title', 'seals', 'description', 'color', 'company_id']
+      })
+        .then(function(card) {
+          if (card) {
+            return cb({result: card, status: 200, success: true, message: '', errors: []});
+          } else {
+            return cb({result: null, status: 404, success: false,
+              message: 'Card not found.', errors: []});
+          }
         })
         .catch(function(err) {
           return cb({result: null, status: 404, success: false,
