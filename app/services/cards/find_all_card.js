b717e1475ae8b2ed928f255d705dc2dd1131ab62
Fher Eduardo <fhereduardo90@gmail.com>
2016-01-31 21:49:18 -0600

    Service to retieve all cards related with a specific company.

---
+++ app/services/cards/find_all_card.js
@@ -0,0 +1,29 @@
+var sequelize     = require('../../models');
+var _             = require('lodash');
+
+module.exports.call = function(companyId, cb) {
+  if (_.isEmpty(companyId)) {
+    companyId = 0;
+  }
+
+  return sequelize.Company.findById(parseInt(companyId), {attributes: ['id']})
+    .then(function(company) {
+      if (_.isEmpty(company)) {
+        return cb({result: null, status: 404, success: false,
+          message: 'Company not found.', errors: []});
+      }
+
+      return company.getCards({attributes: ['title', 'seals', 'description', 'color', 'company_id']})
+        .then(function(cards) {
+          return cb({result: cards, status: 200, success: true, message: '', errors: []});
+        })
+        .catch(function(err) {
+          return cb({result: null, status: 404, success: false,
+            message: 'Card not found.', errors: err.errors});
+        });
+    })
+    .catch(function(err) {
+      return cb({result: null, status: 404, success: false,
+        message: 'Company not found.', errors: err.errors});
+    });
+};
