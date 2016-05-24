72682b6963002e9d4b641e0c7ff71e2d590c4512
= <=>
2016-05-24 09:54:32 -0600

    allow cors

--- app.js
+++ app.js
@@ -31,9 +31,9 @@ app.use(CompanyController);
 app.use(CardController);
 app.use(SessionController);
 
-app.all('/*', function(req, res, next) {
+app.use(function(req, res, next) {
   res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Headers', 'X-Requested-With');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
   res.header('Access-Control-Allow-Methods', 'GET', 'POST','PUT', 'DELETE');
   next();
 });
