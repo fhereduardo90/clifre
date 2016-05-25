6a728463264281ee2aa9540d89a2a9b8c4decbd4
Fher Eduardo <fhereduardo90@gmail.com>
2016-05-25 13:11:12 -0600

    fix error with cors npm

--- app.js
+++ app.js
@@ -19,7 +19,7 @@ var userAuthenticator       = require('./app/middlewares/user_authenticator');
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json({limit: '50mb'}));
-app.use(cors);
+app.use(cors());
 
 app.set('jwtKey', process.env.AUTH0_CLIENT_SECRET); // secret variable
 
