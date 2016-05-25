54618bdf05f92bdcd792e63e8c6bf279be89b7e3
Fher Eduardo <fhereduardo90@gmail.com>
2016-05-25 13:05:35 -0600

    add cors npm

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ var bodyParser      = require('body-parser');
 var port            = process.env.PORT;
 var router          = express.Router();
 var jwt             = require('jsonwebtoken');
+var cors	    = require('cors');
 
 // Controllers
 var UserController      = require('./app/controllers/users');
@@ -18,6 +19,7 @@ var userAuthenticator       = require('./app/middlewares/user_authenticator');
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json({limit: '50mb'}));
+app.use(cors);
 
 app.set('jwtKey', process.env.AUTH0_CLIENT_SECRET); // secret variable
 
@@ -31,11 +33,11 @@ app.use(CompanyController);
 app.use(CardController);
 app.use(SessionController);
 
-app.use(function(req, res, next) {
+/*app.use(function(req, res, next) {
   res.header('Access-Control-Allow-Origin', '*');
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
   res.header('Access-Control-Allow-Methods', 'GET', 'POST','PUT', 'DELETE');
   next();
-});
+});*/
 
 app.listen(port);
