481ade68404bda4781d1f4c82a15086dcd0d4df6
Fher Eduardo <fhereduardo90@gmail.com>
2016-06-04 19:56:57 -0400

    rename functions

--- app/controllers/sessions.js
+++ app/controllers/sessions.js
@@ -3,7 +3,7 @@ var sessionController = new express.Router();
 // Services
 var Authenticate = require('../services/sessions/authenticate');
 var AuthenticateCompany = require('../services/sessions/authenticate_company');
-var AuthenticateUserFacebook = require('../services/sessions/authenticate_user_facebook');
+var AuthenticateFacebookUser = require('../services/sessions/authenticate_facebook_user');
 var CreateUserService = require('../services/users/create_user');
 // Helpers
 var ApiResponse = require('../helpers/api_response');
@@ -34,28 +34,30 @@ sessionController.route('/authenticate-company')
   });
 
 sessionController.route('/authenticate-facebook-user')
-  .post(function authenticateUserFacebook(req, res) {
+  .post(function authenticateFacebookUser(req, res) {
+    // Initialize FacebookApi instance.
     var fb = new FacebookApi(req.body.accessToken);
+
     return fb.me()
-      .then(function meResponse(response) {
+      .then(function success(response) {
         var userParams = {
           facebookId: response.id,
           name: response.name,
           email: response.email
         };
 
-        return AuthenticateUserFacebook.call(userParams.facebookId)
-          .then(function facebookResponse(response) {
+        return AuthenticateFacebookUser.call(userParams.facebookId)
+          .then(function success(response) {
             return ApiResponse.success(res, response);
           })
-          .catch(function facebookError(err) {
+          .catch(function error(err) {
             return CreateUserService.call(userParams);
           });
       })
-      .then(function postUserResponse(response) {
+      .then(function success(response) {
         return ApiResponse.success(res, response);
       })
-      .catch(function postUserError(err) {
+      .catch(function error(err) {
         return ApiResponse.error(res, err);
       });
   })
