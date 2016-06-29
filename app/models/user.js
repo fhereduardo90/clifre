d2a25ff7a7453e09237604c55172cd901e595b50
Fher Eduardo <fhereduardo90@gmail.com>
2016-06-28 23:19:47 -0400

    add resetPasswordToken and resetPasswordExpires fields

--- app/models/user.js
+++ app/models/user.js
@@ -75,6 +75,14 @@ module.exports = function userModel(sequelize, DataTypes) {
         len: { args: [8, 25], msg: 'password only accepts min 8 and max 25 characters.' }
       }
     },
+    resetPasswordToken: {
+      type: DataTypes.STRING,
+      field: 'reset_password_token'
+    },
+    resetPasswordExpires: {
+      type: DataTypes.DATE,
+      field: 'reset_password_expires'
+    },
     facebookId: {
       type: DataTypes.STRING,
       field: 'facebook_id',
