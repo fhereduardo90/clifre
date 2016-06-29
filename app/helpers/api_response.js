242fd6a554ac1ced0638fccf1391b07e755b12ad
Fher Eduardo <fhereduardo90@gmail.com>
2016-06-28 23:18:05 -0400

    add ok function for responding only a status 200

--- app/helpers/api_response.js
+++ app/helpers/api_response.js
@@ -11,3 +11,5 @@ module.exports.success = function (res, params) {
   if (!_.isObject(params)) params = {};
   return res.status(params.status).json(params.result).end();
 };
+
+module.exports.ok = function ok(res) {return res.status(200).end();}
