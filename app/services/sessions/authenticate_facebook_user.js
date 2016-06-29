e69a40a2ab6b5477b891d048052b9dff02dd105a
Fher Eduardo <fhereduardo90@gmail.com>
2016-06-29 00:33:25 -0400

    replace access_token field by accessToken

--- app/services/sessions/authenticate_facebook_user.js
+++ app/services/sessions/authenticate_facebook_user.js
@@ -12,7 +12,7 @@ module.exports.call = function(facebookId) {
       // Generate jwt token.
       var token = jwtTokenGenerator.call({identifier: user.identifier},
         app.get('jwtKey'), '100d');
-      return {result: {access_token: token}, status: 200, success: true,
+      return {result: {accessToken: token}, status: 200, success: true,
         message: 'User has been logged in.', errors: []};
     })
     .catch(function error(err) {
