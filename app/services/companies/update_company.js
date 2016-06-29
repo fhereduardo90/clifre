3551d8a39c3a1ce4401259edeb0fa6435d57a9db
Fher Eduardo <fhereduardo90@gmail.com>
2016-06-29 00:33:00 -0400

    change password validation flow

--- app/services/companies/update_company.js
+++ app/services/companies/update_company.js
@@ -1,10 +1,18 @@
-var sequelize         = require('../../models');
-var _                 = require('lodash');
-var errorParse        = require('../../helpers/error_parse');
-var ApiError          = require('../../errors/api_error');
-var UploaderAvatar    = require('../../helpers/uploader_avatar');
-var Promise           = require('bluebird');
+var sequelize = require('../../models');
+var _ = require('lodash');
+var errorParse = require('../../helpers/error_parse');
+var ApiError = require('../../errors/api_error');
+var UploaderAvatar = require('../../helpers/uploader_avatar');
+var Promise = require('bluebird');
 
+/**
+* Upload company avatar to S3 and saving it in a specefic path.
+*
+* @param {string} avatar, the base64 that will be uploaded to S3.
+* @param {string} path, the place where the avatar will be located.
+* @param {Object} company, the instance of the current user.
+* @returns {Promise} Returns an UploaderAvatar promise.
+*/
 function uploadAvatar (avatar, path, company) {
   // Initializer UploaderAvatar instance and upload company avatar
   var UploaderCompanyAvatar = new UploaderAvatar(path);
@@ -31,20 +39,18 @@ function uploadAvatar (avatar, path, company) {
 module.exports.call = function(company, params) {
   return new Promise.try(function () {
     try {
-      company.temporalPassword = params.password;
-      return company.update(_.omit(params, ['avatar']))
-        .then(function (c) {
+      return company.update(params)
+        .then(function success(c) {
           var path = 'companies/' + company.identifier + '/avatar';
           if (params.avatar) return uploadAvatar(params.avatar, path, company);
           return c;
         })
-        .then(function (c) {
+        .then(function success(c) {
           var response = _.pick(c, ['id', 'name', 'email', 'identifier', 'about',
             'address', 'phone', 'avatar']);
-          return {result: response, status: 200, success: true,
-            message: 'Company has been updated.', errors: []};
+          return {result: response, status: 200, success: true};
         })
-        .catch(function (err) {
+        .catch(function error(err) {
           throw new ApiError('Company could not be updated.', 422, errorParse(err));
         });
     } catch (e) {
