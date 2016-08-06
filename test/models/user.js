5ccdc164d5f2f2ca995fa816b8ed704ebb3852eb
Fher Eduardo <fhereduardo90@gmail.com>
2016-08-06 01:12:23 -0400

    ladies and gentlemen we already have TDD for models :tada:

--- test/models/user.js
+++ test/models/user.js
@@ -5,40 +5,45 @@ var expect = require('chai').expect;
 var errorParse = require('../../app/helpers/error_parse');
 
 var User = sequelize.User;
+
+// VALIDATIONS
+
 var testUser;
 var savedUser;
 var newUser;
 var params;
-var transaction;
+
+// CRUD OPERATIONS
+var user1, user2, user3;
 
 describe('User Model', function test() {
 
-  before(function test(done) {
-    params = {
-      name: faker.name.findName(),
-      password: faker.internet.password(),
-      email: faker.internet.email(),
-      birthdate: faker.date.past(),
-      identifier: faker.random.uuid(),
-      facebookId: faker.random.uuid()
-    };
-
-    savedUser = User.build(params);
-
-    savedUser.save()
-      .then(function success(user) {
-        done();
-      }).catch(function err(error) {
-        done(error);
-      });
-  });
-
-  beforeEach(function test() {
-    testUser = User.build(params);
-    newUser = User.build(params);
-  });
-
   describe('Validations', function test() {
+
+    before(function test(done) {
+      params = {
+        name: faker.name.findName(),
+        password: faker.internet.password(),
+        email: faker.internet.email(),
+        birthdate: faker.date.past(),
+        identifier: faker.random.uuid(),
+        facebookId: faker.random.uuid()
+      };
+
+      savedUser = User.build(params);
+
+      savedUser.save()
+        .then(function success(user) {
+          done();
+        }).catch(function err(error) {
+          done(error);
+        });
+    });
+
+    beforeEach(function test() {
+      testUser = User.build(params);
+      newUser = User.build(params);
+    });
 
     it('name should not be blank', function test(done) {
       testUser.name = '';
@@ -196,11 +201,9 @@ describe('User Model', function test() {
     });
 
     it('email can be blank if there is a facebookId assigned', function test(done) {
-      testUser.email = null;
-      testUser.facebookId = null;
+      testUser.email = '';
       testUser.validate()
         .then(function success(result) {
-          console.dir(errorParse(result));
           expect(errorParse(result)).to.not.contain('email cannot be blank');
           done();
         })
@@ -220,13 +223,125 @@ describe('User Model', function test() {
         });
     });
 
+    after(function test(done) {
+      savedUser.destroy()
+        .then(function success() {
+          done();
+        });
+    });
+
   });
 
-  after(function test(done) {
-    savedUser.destroy()
-      .then(function success() {
-        done();
+  describe('CRUD Operations', function test() {
+
+    before(function test(done) {
+      user1 = User.build({
+        name: faker.name.findName(),
+        password: faker.internet.password(),
+        email: faker.internet.email(),
+        birthdate: faker.date.past(),
+        identifier: faker.random.uuid(),
+        facebookId: faker.random.uuid(),
+        avatar: faker.image.imageUrl(),
+        avatarName: faker.random.uuid()
       });
+
+      user2 = User.build({
+        name: faker.name.findName(),
+        password: faker.internet.password(),
+        email: faker.internet.email(),
+        birthdate: faker.date.past(),
+        identifier: faker.random.uuid(),
+        facebookId: faker.random.uuid(),
+        avatar: faker.image.imageUrl(),
+        avatarName: faker.random.uuid()
+      });
+
+      user3 = User.build({
+        name: faker.name.findName(),
+        password: faker.internet.password(),
+        email: faker.internet.email(),
+        birthdate: faker.date.past(),
+        identifier: faker.random.uuid(),
+        facebookId: faker.random.uuid(),
+        avatar: faker.image.imageUrl(),
+        avatarName: faker.random.uuid()
+      });
+
+      user1.save()
+        .then(function success() {
+          return user2.save();
+        })
+        .then(function success() {
+          done();
+        })
+        .catch(function err(error) {
+          done(error);
+        });
+    });
+
+    it('create new user', function test(done) {
+      user3.save()
+        .then(function success() {
+          done();
+        })
+        .catch(function err(error) {
+          done(error);
+        });
+    });
+
+    it('update existing user', function test(done) {
+      user3.update({
+        name: faker.name.findName(),
+        password: faker.internet.password(),
+        email: faker.internet.email(),
+        birthdate: faker.date.past(),
+        identifier: faker.random.uuid(),
+        facebookId: faker.random.uuid(),
+        avatar: faker.image.imageUrl(),
+        avatarName: faker.random.uuid()
+      }).then(function success() {
+        done();
+      }).catch(function err(error) {
+        done(error);
+      });
+    });
+
+    it('delete user', function test(done) {
+      user3.destroy()
+        .then(function success(user) {
+          if (user === 0) done(new Error('User could not be deleted'));
+          else done();
+        })
+        .catch(function err(error) {
+          done(error)
+        });
+    });
+
+    it('select users', function test(done) {
+      User.findAll({attributes: ['id']})
+        .then(function success(users) {
+          if (users.length === 0) done(new Error('Users could not be found'));
+          else done();
+        })
+        .catch(function err(error) {
+          done(error)
+        });
+    });
+
+    after(function test(done) {
+      user1.destroy()
+        .then(function success() {
+          return user2.destroy();
+        })
+        .then(function success() {
+          done();
+        })
+        .catch(function err(error) {
+          done(error);
+        });
+    });
+
   });
 
 });
