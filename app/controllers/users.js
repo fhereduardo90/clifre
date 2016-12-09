8a698c9ab3e4d0237e0897a291fd3eea2e253571
Fher Eduardo <fhereduardo90@gmail.com>
2016-12-08 19:35:29 -0600

    add new route "POST /users/me/device"

--- app/controllers/users.js
+++ app/controllers/users.js
@@ -14,55 +14,69 @@ var express = require('express');
 var userController = new express.Router();
 
 userController.route('/users')
-  .get(function getUsers(req, res) {
-    return AllUsersService.call()
-      .then(function getUsersResponse(response) {
-        return ApiResponse.success(res, response);
-      })
-      .catch(function getUsersError(err) {
-        return ApiResponse.error(res, err);
-      });
-  })
+	.get(function getUsers(req, res) {
+		return AllUsersService.call()
+			.then(function getUsersResponse(response) {
+				return ApiResponse.success(res, response);
+			})
+			.catch(function getUsersError(err) {
+				return ApiResponse.error(res, err);
+			});
+	})
 
-  .post(function postUser(req, res) {
-    var userParams = _.pick(req.body, ['name', 'email', 'birthdate',
-      'password', 'avatar']);
-    return CreateUserService.call(userParams)
-      .then(function postUserResponse(response) {
-        return ApiResponse.success(res, response);
-      })
-      .catch(function postUserError(err) {
-        return ApiResponse.error(res, err);
-      });
-  });
+	.post(function postUser(req, res) {
+		var userParams = _.pick(req.body, ['name', 'email', 'birthdate',
+			'password', 'avatar']);
+		return CreateUserService.call(userParams)
+			.then(function postUserResponse(response) {
+				return ApiResponse.success(res, response);
+			})
+			.catch(function postUserError(err) {
+				return ApiResponse.error(res, err);
+			});
+	});
 
 userController.route('/users/me')
-  .get(userAuthenticator, function getUserProfile(req, res) {
-    var attrs = ['id', 'name', 'identifier', 'email', 'birthdate', 'avatar'];
-    return res.json(_.pick(req.user, attrs));
-  })
+	.get(userAuthenticator, function getUserProfile(req, res) {
+		var attrs = ['id', 'name', 'identifier', 'email', 'birthdate', 'avatar'];
+		return res.json(_.pick(req.user, attrs));
+	})
 
-  .put(userAuthenticator, function putUser(req, res) {
-    var userParams = _.pick(req.body, ['name', 'email', 'birthdate',
-      'password', 'avatar']);
-    return UpdateUserService.call(req.user, userParams)
-      .then(function putUserResponse() {
-        return ApiResponse.ok(res);
-      })
-      .catch(function putUserError(err) {
-        return ApiResponse.error(res, err);
-      });
-  });
+	.put(userAuthenticator, function putUser(req, res) {
+		var userParams = _.pick(req.body, ['name', 'email', 'birthdate',
+			'password', 'avatar']);
+		return UpdateUserService.call(req.user, userParams)
+			.then(function putUserResponse() {
+				return ApiResponse.ok(res);
+			})
+			.catch(function putUserError(err) {
+				return ApiResponse.error(res, err);
+			});
+	});
 
-  userController.route('/users/:identifier')
-    .get(function done(req, res) {
-      return FindUserService.call({identifier: req.params.identifier})
-        .then(function success(response) {
-          return ApiResponse.success(res, response);
-        })
-        .catch(function error(err) {
-          return ApiResponse.error(res, err);
-        });
-    });
+userController.route('/users/:identifier')
+	.get(function done(req, res) {
+		return FindUserService.call({identifier: req.params.identifier})
+			.then(function success(response) {
+				return ApiResponse.success(res, response);
+			})
+			.catch(function error(err) {
+				return ApiResponse.error(res, err);
+			});
+	});
+
+userController.route('/users/me/device')
+	.post(function done(req, res) {
+		return AddUserDevice.call(req.user, {
+		  registrationId: req.body.registrationId,
+			platform: req.body.platform
+		})
+      .then(function success(response) {
+
+		  })
+      .catch(function error(err) {
+
+		  });
+	});
 
 module.exports = userController;
