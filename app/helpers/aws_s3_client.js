d8a9a93431291e9b2abe26203de80912efeb47b8
Fher Eduardo <fhereduardo90@gmail.com>
2017-07-17 10:26:22 -0600

    Use signature version 2

--- app/helpers/aws_s3_client.js
+++ app/helpers/aws_s3_client.js
@@ -7,7 +7,9 @@ function S3Client () {
     accessKeyId: process.env.AWS_ACCESS_KEY_ID,
     secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY
   });
-  return new AWS.S3();
+  return new AWS.S3({
+    signatureVersion: 'v2'
+  });
 }
 
 module.exports = S3Client();
