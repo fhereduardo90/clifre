6e973d917064f250fa6c7ed617405d8d7bc4959e
Fher Eduardo <fhereduardo90@gmail.com>
2018-01-20 18:17:34 -0600

    endpoints for categories must be public

--- app/controllers/categories.js
+++ app/controllers/categories.js
@@ -1,19 +1,15 @@
-// Middlewares
-const CompanyAuthenticator = require('../middlewares/company_authenticator');
-// Helpers
 const ApiResponse = require('../helpers/api_response');
-// Services
 const AllCategoriesService = require('../services/categories/all_categories');
 const FindCategoryService = require('../services/categories/find_category');
 const categoryController = require('express').Router();
 
-categoryController.route('/categories').get(CompanyAuthenticator, (req, res) => {
+categoryController.route('/categories').get((req, res) => {
   AllCategoriesService.call()
     .then(response => ApiResponse.success(res, response))
     .catch(err => ApiResponse.error(res, err));
 });
 
-categoryController.route('/categories/:id').get(CompanyAuthenticator, (req, res) => {
+categoryController.route('/categories/:id').get((req, res) => {
   FindCategoryService.call(parseInt(req.params.id, 10))
     .then(response => ApiResponse.success(res, response))
     .catch(err => ApiResponse.error(res, err));
