5442b6b49b8ae4b4b67f0bff70372e4f1e43c809
Fher Eduardo <fhereduardo90@gmail.com>
2018-02-03 15:30:26 -0600

    remove console.log

--- app/services/cards/update_card.js
+++ app/services/cards/update_card.js
@@ -10,16 +10,21 @@ const ApiError = require('../../errors/api_error');
 module.exports.call = (company, id, params) => {
   return Promise.try(() => {
     try {
-      if (!id || !Number.isInteger(id)) throw new Error('Parameters are incorrect.');
-      return company.getCards({ where: { id } })
-        .then((cards) => {
-          console.log(params);
+      if (!id || !Number.isInteger(id))
+        throw new Error('Parameters are incorrect.');
+      return company
+        .getCards({ where: { id } })
+        .then(cards => {
           if (_.isEmpty(cards)) throw new Error('Card not found.');
           return cards[0].update(params);
         })
         .then(() => ({ result: null, status: 204 }))
-        .catch((err) => {
-          throw new ApiError('Card could not be updated.', 422, errorParse(err));
+        .catch(err => {
+          throw new ApiError(
+            'Card could not be updated.',
+            422,
+            errorParse(err),
+          );
         });
     } catch (err) {
       throw new ApiError('Card could not be updated.', 422, errorParse(err));
