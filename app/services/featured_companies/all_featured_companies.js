5729d8bafd7a564115a32f587899055060ce58f0
Lisandro Lopez <llopez@phunware.com>
2018-03-14 17:08:31 -0600

    Fixing featured companies

--- app/services/featured_companies/all_featured_companies.js
+++ app/services/featured_companies/all_featured_companies.js
@@ -8,36 +8,20 @@ const ApiError = require('../../errors/api_error');
 
 module.exports.call = async () => {
   try {
-    const featuredCompanies = await sequelize.FeaturedCompany.findAll({
-            order: '"createdAt" DESC',
-            include: [
-              {
-                model: sequelize.Company,
-                include: [
-                  { model: sequelize.Category },
-                  { model: sequelize.Country },
-                ],
-              },
-            ],
-          });
-
-          return {
-                  result: [
-                    ...featuredCompanies.map(({ Company = {}, Company: { Category, Country } = {}, ...fc }) => ({
-                      ...FeaturedCompanyDetailSerializer.serialize(fc),
-                      company: {
-                        ...CompanyDetailSerializer.serialize(Company),
-                        ...(Category
-                          ? { category: CategoryDetailSerializer.serialize(Category) }
-                          : {}),
-                        ...(Country
-                          ? { country: CountryDetailSerializer.serialize(Country) }
-                          : {}),
-                      },
-                    })),
-                  ],
-                  status: 200,
-                };
+    return sequelize.FeaturedCompany.findAll({ order: '"createdAt" DESC', include: [{ model: sequelize.Company }] })
+        .then(featuredCompanies => ({
+          result: featuredCompanies.map((featuredCompany) => {
+            return Object.assign(
+              {},
+              FeaturedCompanyDetailSerializer.serialize(featuredCompany),
+              { company: CompanyDetailSerializer.serialize(featuredCompany.Company) }
+            );
+          }),
+          status: 200,
+        }))
+        .catch((err) => {
+          throw new ApiError('Featured Companies not be found.', 404, errorParse(err));
+        });
   } catch (err) {
     throw new ApiError('Featured Companies not found.', 422, errorParse(err));
   }
